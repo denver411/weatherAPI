9d1de23dbb0b4d02c70108fd4d41b6ad758cc703
Denis Zhebov <zhebov@gmail.com>
2018-09-24 22:35:30 +0300

    Добавил прогноз на 3 дня

--- script.js
+++ script.js
@@ -16,25 +16,30 @@ const Weather = ({ data }) => {
       <h2 className="weather__city">{data.city.name}</h2>
       <span className="weather__date">now</span>
       <div className="weather__today">
-      <span className="weather__temp">
-        {data.list[0].main.temp.toFixed(1)}
-        &deg;
-      </span>
-      <img
-        className="weather__icon"
-        src={`https://openweathermap.org/img/w/${
-          data.list[0].weather[0].icon
-        }.png`}
-      />
-      <span className="weather__description">
-        {data.list[0].weather[0].main}
-      </span>
+        <span className="weather__temp">
+          {data.list[0].main.temp.toFixed(1)}
+          &deg;
+        </span>
+        <img
+          className="weather__icon"
+          src={`https://openweathermap.org/img/w/${
+            data.list[0].weather[0].icon
+          }.png`}
+        />
+        <span className="weather__description">
+          {data.list[0].weather[0].main}
+        </span>
+      </div>
+      <div className="weather__forecast">
+      <WeatherBlock data={data.list[8]}/>
+      <WeatherBlock data={data.list[16]}/>
+      <WeatherBlock data={data.list[24]}/>
       </div>
     </div>
   );
 };
 
-function getWeather(data) {
+const getWeather = data => {
   const place = data.lat ? `lat=${data.lat}&lon=${data.lon}` : `q=${data}`;
   console.log(place);
   fetch(
@@ -49,13 +54,36 @@ function getWeather(data) {
       );
     })
     .catch(console.log);
-}
+};
 
-function newWeather(event) {
+const newWeather = event => {
   event.preventDefault();
   getWeather(event.currentTarget.elements[0].value);
-}
+};
 
+const WeatherBlock = ({ data }) => {
+  console.log (data);
+  const weekDay = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday"
+  };
+  return (
+    <div className="block">
+      <h3 className="block__title">{weekDay[new Date(data.dt_txt).getDay()]}</h3>
+      <img
+        className="block__icon"
+        src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+      />
+      <span className="block__temp">{data.main.temp_min.toFixed(0)}&deg; / {data.main.temp_max.toFixed(0)}&deg;</span>
+      <span className="block__description">{data.weather[0].main}</span>
+    </div>
+  );
+};
 // document.addEventListener('DOMContentLoaded', getWeather('Saint Petersburg'));
 
 navigator.geolocation.getCurrentPosition(function(position) {
