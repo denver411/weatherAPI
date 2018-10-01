925181593fc2f9dfbfe212d0139b2b7070e63c48
Denis Zhebov <zhebov@gmail.com>
2018-10-01 21:56:50 +0300

    Add temperaature range

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// const place = {city: "Saint Petersburg", geo: null};
-
 const Weather = ({ data }) => {
+  const startListItem = (24 - new Date(data.list[0].dt_txt).getHours()) / 3;
+
   return (
     <div className="weather">
       <form onSubmit={newWeather} className="weather__search-form">
@@ -13,8 +13,8 @@ const Weather = ({ data }) => {
           placeholder="Search another city"
         />
       </form>
-      <h2 className="weather__city">{data.city.name}</h2>
-      <span className="weather__date">now</span>
+      <h2 className="weather__city"> {data.city.name} </h2>
+      <span className="weather__date"> now </span>
       <div className="weather__today">
         <span className="weather__temp">
           {data.list[0].main.temp.toFixed(1)}
@@ -31,9 +31,9 @@ const Weather = ({ data }) => {
         </span>
       </div>
       <div className="weather__forecast">
-      <WeatherBlock data={data.list[8]}/>
-      <WeatherBlock data={data.list[16]}/>
-      <WeatherBlock data={data.list[24]}/>
+        <WeatherBlock data={data.list} startItem={startListItem} />
+        <WeatherBlock data={data.list} startItem={startListItem + 8} />
+        <WeatherBlock data={data.list} startItem={startListItem + 16} />
       </div>
     </div>
   );
@@ -61,34 +61,53 @@ const newWeather = event => {
   getWeather(event.currentTarget.elements[0].value);
 };
 
-const WeatherBlock = ({ data }) => {
-  console.log (data);
+const WeatherBlock = ({ data, startItem }) => {
+
+  let minTemp = data[startItem].main.temp_min;
+  let maxTemp = data[startItem].main.temp_max;
+  for (let i = (startItem + 1); i < (startItem + 8); i++) {
+    minTemp = Math.min(minTemp, data[i].main.temp_min);
+    maxTemp = Math.max(maxTemp, data[i].main.temp_max);
+  }
+
   const weekDay = {
-    0: "Sunday",
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday"
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
   };
-  const dateWeek = new Date(data.dt_txt);
+  const dateWeek = new Date(data[startItem].dt_txt);
   return (
     <div className="block">
-      <h3 className="block__title">{weekDay[dateWeek.getDay()]}</h3>
+      <h3 className="block__title"> {weekDay[dateWeek.getDay()]} </h3>
       <img
         className="block__icon"
-        src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+        src={`https://openweathermap.org/img/w/${
+          data[startItem].weather[0].icon
+        }.png`}
       />
-      <span className="block__temp">{data.main.temp_min.toFixed(0)}&deg; / {data.main.temp_max.toFixed(0)}&deg;</span>
-      <span className="block__description">{data.weather[0].main}</span>
+      <span className="block__temp">
+        {minTemp.toFixed(0)}
+        &deg; / {maxTemp.toFixed(0)}
+        &deg;
+      </span>
+      <span className="block__description">{data[startItem].weather[0].main}</span>
     </div>
   );
 };
 // document.addEventListener('DOMContentLoaded', getWeather('Saint Petersburg'));
 
-navigator.geolocation.getCurrentPosition(function(position) {
-  const geo = { lat: position.coords.latitude, lon: position.coords.longitude };
-  getWeather(geo);
-  // console.log(position.coords.latitude, position.coords.longitude);
-}, () => getWeather("Saint Petersburg"));
+navigator.geolocation.getCurrentPosition(
+  function(position) {
+    const geo = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude
+    };
+    getWeather(geo);
+    // console.log(position.coords.latitude, position.coords.longitude);
+  },
+  () => getWeather('Saint Petersburg')
+);
